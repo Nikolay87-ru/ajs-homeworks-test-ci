eb3bf6527cb0b27590f124ee6e403c401bde53c0
Neto test <nikolajeliseev834@gmail.com>
2025-04-21 17:36:24 +0300

    Добавлена функция проверки здоровья персонажа в app.js, создан файл app.test.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from "./basic";
 
-console.log('worked');
+// console.log("worked");
 
-console.log(sum([1, 2]));
+// console.log(sum([1, 2]));
+
+/**
+ * @param {Object} - {name: 'Маг', health: 90}
+ */
+
+export function healthStatus({ _, health}) {
+  if (health > 50) {
+    return "healthy";
+  }
+  if (health >= 15) {
+    return "wounded";
+  }
+  return "critical";
+}
