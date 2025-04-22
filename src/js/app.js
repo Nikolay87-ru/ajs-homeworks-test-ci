39aed3fcb8bb07ad6802e17501d2795d62d902b6
Neto test <nikolajeliseev834@gmail.com>
2025-04-22 17:21:54 +0300

    Добавлена функция из 3-ей задачи

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 // TODO: write your code here
+import fetchData from './http';
 
 export function healthStatus(character) {
   if (character.health > 50) {
@@ -12,4 +13,14 @@ export function healthStatus(character) {
 
 export function sortingListOfHeroes(heroes) {
   return heroes.sort((x, y) => x.health - y.health).reverse();
+}
+
+export function getLevel(userId) {
+  const response = fetchData(`https://server/user/${userId}`);
+  
+  if (response.status === 'ok') {
+     return `Ваш текущий уровень: ${response.level}`; 
+  }
+  
+  return `Информация об уровне временно недоступна`;
 }
